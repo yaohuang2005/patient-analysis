e8d1f7862d435dec62099089f5659ec90c5767b9
Yao Huang <yhuang@trustwave.com>
2016-01-17 16:59:27 -0500

    print numpy

---
+++ main.py
@@ -0,0 +1,44 @@
+import numpy
+data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
+print(data)
+print(data.mean())
+print('maximum inflammation:', data.max())
+print('minimum inflammation:', data.min())
+print('standard deviation:', data.std())
+
+patient_0 = data[0, :] # 0 on the first axis, everything on the second
+print('maximum inflammation for patient 0:', patient_0.max())
+
+
+print(data.mean(axis=0))
+
+print(data.mean(axis=1))
+
+import matplotlib.pyplot
+image  = matplotlib.pyplot.imshow(data)
+matplotlib.pyplot.show(image)
+
+ave_inflammation = data.mean(axis=0)
+ave_plot = matplotlib.pyplot.plot(ave_inflammation)
+matplotlib.pyplot.show(ave_plot)
+
+
+
+fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+
+axes1 = fig.add_subplot(1, 3, 1)
+axes2 = fig.add_subplot(1, 3, 2)
+axes3 = fig.add_subplot(1, 3, 3)
+
+axes1.set_ylabel('average')
+axes1.plot(data.mean(axis=0))
+
+axes2.set_ylabel('max')
+axes2.plot(data.max(axis=0))
+
+axes3.set_ylabel('min')
+axes3.plot(data.min(axis=0))
+
+fig.tight_layout()
+
+matplotlib.pyplot.show(fig)
