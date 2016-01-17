ca3fec9e8ccb14adca7fdf88535fbfbb156a0f09
Yao Huang <yhuang@trustwave.com>
2016-01-17 17:09:58 -0500

    read multiple files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy
+import matplotlib.pyplot
+import glob
 data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
 print(data)
 print(data.mean())
@@ -14,7 +16,6 @@ print(data.mean(axis=0))
 
 print(data.mean(axis=1))
 
-import matplotlib.pyplot
 image  = matplotlib.pyplot.imshow(data)
 matplotlib.pyplot.show(image)
 
@@ -42,3 +43,29 @@ axes3.plot(data.min(axis=0))
 fig.tight_layout()
 
 matplotlib.pyplot.show(fig)
+
+
+filenames = glob.glob('data/inflammation*.csv')
+filenames = filenames[0:3]
+for f in filenames:
+    print(f)
+
+    data = numpy.loadtxt(fname=f, delimiter=',')
+
+    fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
+
+    axes1.set_ylabel('average')
+    axes1.plot(data.mean(axis=0))
+
+    axes2.set_ylabel('max')
+    axes2.plot(data.max(axis=0))
+
+    axes3.set_ylabel('min')
+    axes3.plot(data.min(axis=0))
+
+    fig.tight_layout()
+    matplotlib.pyplot.show(fig)
